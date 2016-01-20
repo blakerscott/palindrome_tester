6869b05caecac1e3805417f12d7322f60aab0f4e
Blake Scott and James Turello <blakerscott@gmail.com>
2016-01-20 15:29:44 -0800

    create first test script to reverse string input

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var palindrome = function(str) {
+
+    // var strArray = str.split('');
+    // var reversedArray = strArray.reverse();
+    // var reversedStr = reversedArray.join('');
+
+    return str.split('').reverse().join('');
+
+}
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('palindrome', function() {
+  it("will reverse the characters of the input", function() {
+    expect(palindrome("bear")).to.equal("raeb");
+  });
+});
